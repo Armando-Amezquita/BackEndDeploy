0f92ee6805ff50524bba7cb2f5121c28156f96bf
Armando Amezquita <a.amezquita.1009@gmail.com>
2022-01-25 22:15:07 -0500

    Prueba Heroku

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const {
 //   DB_USER=RedSocial
 // DB_PASSWORD=RedSocial
 // DB_NAME=RedSocial
-  DB_USER, DB_PASSWORD, DB_NAME,
+  DB_USER, DB_PASSWORD, DB_NAME, MONGO_URI
 } = process.env;
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@redsocial.n72mr.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, {useNewUrlParser: true})
+mongoose.connect(`${MONGO_URI}`, {useNewUrlParser: true})
 .then((db) => console.log(`Mongo DB has been conected in: ${db.connection.name}`))
 .catch(err=>console.log(`This error has been interupt: \n${err}`))
